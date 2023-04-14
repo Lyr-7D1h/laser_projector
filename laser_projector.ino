9ee0f995455a1d74e2467ed4a5e7e111602e7361
Lyr <lyr-7d1h@pm.me>
2023-04-14 02:49:24 +0200

    added models and gcodes

--- laser_projector.ino
+++ laser_projector.ino
@@ -1,32 +1,17 @@
-const int galvo_x_pin = 25;
-const int galvo_y_pin = 26;
-const int delay_ms = 5;
+const int galvo_y_pin = 25;
+const int galvo_x_pin = 26;
+const int delay_ms = 1;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(galvo_x_pin, OUTPUT);
   pinMode(galvo_y_pin, OUTPUT);
+  pinMode(galvo_x_pin, OUTPUT);
 }
 
 void loop() {
  while (true) {
-   for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
-    analogWrite(galvo_x_pin, dutyCycle);
-    delay(delay_ms); 
-   }
-
-   for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
-    analogWrite(galvo_x_pin, dutyCycle);
-    delay(delay_ms); 
-   }
-
-   for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
-    analogWrite(galvo_y_pin, dutyCycle);
-    delay(delay_ms); 
-   }
-
-   for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
-    analogWrite(galvo_y_pin, dutyCycle);
+   for (int dutyCycle = 0; dutyCycle <= 10000; dutyCycle++) {
+    analogWrite(galvo_x_pin, dutyCycle / 255);
     delay(delay_ms); 
    }
  }
